161e738ecd9772038ffb4195cfb2406d045f7408
Ricardo Pereira <m@ricardopereira.eu>
2016-03-10 12:16:44 +0000

    Init

--- CrashTestSuiteExampleTests/CrashTestSuiteExampleTests.m
+++ CrashTestSuiteExampleTests/CrashTestSuiteExampleTests.m
@@ -16,24 +16,40 @@
 
 - (void)setUp {
     [super setUp];
-    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown {
-    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
-- (void)testExample {
-    // This is an example of a functional test case.
-    // Use XCTAssert and related functions to verify your tests produce the correct results.
+- (void)testFailure {
+    XCTestExpectation *expectation = [self expectationWithDescription:@"wait"];
+
+    [self doSomethingAsync:false callback:^{
+        NSAssert(false, @"Done");
+        [expectation fulfill];
+    }];
+    
+    [self waitForExpectationsWithTimeout:5.0 handler:nil];
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
+- (void)doSomethingAsync:(BOOL)performBlock callback:(void (^)())callback {
+    __block CFRunLoopRef rl = CFRunLoopGetCurrent();
+
+    NSURLSession *session = [NSURLSession sharedSession];
+    [[session dataTaskWithURL:[NSURL URLWithString:@"http://google.com/?q=crash"] completionHandler:^(NSData *data, NSURLResponse *response, NSError *error) {
+
+        if (performBlock) {
+            CFRunLoopPerformBlock(rl, kCFRunLoopDefaultMode, ^{
+                callback();
+            });
+        }
+        else {
+            callback();
+        }
+
+        CFRunLoopWakeUp(rl);
+    }] resume];
 }
 
 @end
