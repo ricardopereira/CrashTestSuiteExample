f6d983ca58e640bf48a675fb33c46ddd103ac783
Ricardo Pereira <m@ricardopereira.eu>
2016-03-10 11:21:54 +0000

    Initial Commit

---
+++ CrashTestSuiteExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CrashTestSuiteExample
+//
+//  Created by Ricardo Pereira on 10/03/16.
+//  Copyright © 2016 Ricardo Pereira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CrashTestSuiteExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CrashTestSuiteExample
+//
+//  Created by Ricardo Pereira on 10/03/16.
+//  Copyright © 2016 Ricardo Pereira. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
